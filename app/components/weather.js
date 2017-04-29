b8611b890c8d910b471186cf894adb5051052708
Alec Jandorek <ajandorek@gmail.com>
2017-04-29 10:54:21 -0500

    fixed weather bug

--- app/components/weather.js
+++ app/components/weather.js
@@ -18,10 +18,10 @@ class Weather extends Component {
 
     render() {
         if (this.props.weather.isFetching) {
-            return (<div style={style}><Halogen.PacmanLoader color='#4DAF7C'/></div>)
+            return (<div><Halogen.PacmanLoader color='#4DAF7C'/></div>)
         }
         if (!this.props.weather.data) {
-            return (<div style={style}><Halogen.PacmanLoader color='#4DAF7C'/></div>)
+            return (<div><Halogen.PacmanLoader color='#4DAF7C'/></div>)
         }
         return (
             <div className="well">
